319ca70a61a28855a7259409d62194c7e59510fd
koenschuite <tse.lao@yahoo.com>
2023-10-20 18:40:15 +0200

    change background

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-background-base text-primary`} suppressHydrationWarning>
+      <body className={`${inter.className} bg-gradient-to-b from-state-info to-accent-secondary text-primary`} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
