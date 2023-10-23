b18a873710f15cd12e69fee47a04f96eb2ef750e
koenschuite <tse.lao@yahoo.com>
2023-10-23 15:55:39 +0200

    remove a in link

--- app/project/project-item.tsx
+++ app/project/project-item.tsx
@@ -4,6 +4,7 @@ import { LocateFixedIcon, UserCheck } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useAccount } from "wagmi";
+
 interface ProjectItemProps {
     id: string;
     name: string;
@@ -14,41 +15,44 @@ interface ProjectItemProps {
     creator: string;
     members: any[];
 }
-export default function ProjectItem(
-    {details }: { details: ProjectItemProps}
-) {
-    const {address} = useAccount();
-    console.log(details);
-  return (
-    <Link href={`/project/${details.id}`}>
-    <Card className="flex flex-row gap-4">
-        <Image
-            src={`https://ipfs.io/ipfs/${details.image}`}
-            alt={details.name}
-            width={100}
-            height={100}
-            className="rounded-full bg-accent-secondary"
-        />
-        <section className="flex flex-col gap-2 w-full prose">
-            <header className="flex justify-between items-center prose">
-                <h5>{details.name}</h5>
-    
-                {details.creator == address?.toLowerCase() &&  <Badge variant={"info"} className="ml-2">Owner</Badge>}
-            </header>
-            <div className="flex gap-8">
-                <address className="flex gap-2 text-text-secondary text-sm items-center">
-                    <LocateFixedIcon />
-                </address>
-                <div className="flex gap-2 text-text-secondary text-sm items-center">
-                    <UserCheck />
-                    {details.members?.length} Members
-                </div>
-            </div>
-            <p >
-                {details.description ? details.description : "Random descripotion"}
-            </p>
-        </section>
-    </Card>
-    </Link>
-  )
+
+const DEFAULT_DESCRIPTION = "Random description";
+
+export default function ProjectItem({ details }: { details: ProjectItemProps }) {
+    const { address } = useAccount();
+    const normalizedAddress = address?.toLowerCase();
+
+    return (
+        <Link href={`/project/${details.id}`}>
+            <Card className="flex flex-row gap-4">
+                <figure className="relative h-20 w-20 rounded-full bg-accent-secondary object-fit">
+                    <Image
+                        src={`https://ipfs.io/ipfs/${details.image}`}
+                        alt={`Project ${details.name}`}
+                        layout="fill"
+                        className="rounded-full"
+                    />
+                </figure>
+                <section className="flex flex-col gap-2 w-full prose">
+                    <header className="flex justify-between items-center prose">
+                        <h5>{details.name}</h5>
+                        {details.creator === normalizedAddress && <Badge variant="info" className="ml-2">Owner</Badge>}
+                    </header>
+                    <div className="flex gap-8">
+                        <span className="flex gap-2 text-text-secondary capitalize first-line:text-sm items-center">
+                            <LocateFixedIcon />
+                            {details.location}
+                        </span>
+                        <div className="flex gap-2 text-text-secondary text-sm items-center">
+                            <UserCheck />
+                            {details.members?.length} Members
+                        </div>
+                    </div>
+                    <p>
+                        {details.description || DEFAULT_DESCRIPTION}
+                    </p>
+                </section>
+            </Card>
+        </Link>
+    );
 }
