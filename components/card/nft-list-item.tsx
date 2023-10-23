c51f32165f076b04585988a9b8e5ecb267da9377
koenschuite <tse.lao@yahoo.com>
2023-10-23 16:55:54 +0200

    nft item optimised

--- components/card/nft-list-item.tsx
+++ components/card/nft-list-item.tsx
@@ -1,4 +1,3 @@
-"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
@@ -10,9 +9,9 @@ export interface NFTItem {
   contract: string;
   id: string;
   tokenId: number;
-  group:{
+  group: {
     id: any;
-  }
+  };
   metadata: {
     name: string;
     image: string;
@@ -24,6 +23,7 @@ export interface NFTItem {
     }[];
   };
 }
+
 interface NftListItemProps {
   showSelect?: boolean;
   selected?: boolean;
@@ -35,68 +35,52 @@ export default function NftListItem({
   selected,
   item
 }: NftListItemProps) {
-  const searchParams = useSearchParams()!
-  const router = useRouter()
-  const pathname = usePathname()
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const DEFAULT_IPFS_URL = "https://ipfs.io/ipfs/";
 
   const createQueryString = useCallback(
     (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams)
-      params.set(name, value)
- 
-      return params.toString()
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+      return params.toString();
     },
     [searchParams]
-  )
-  
-  
+  );
+
   function removeIPFSPrefix(url: string): string {
-    const prefix = "https://ipfs.io/ipfs/";
-    if (url.startsWith(prefix)) {
-        return url.substring(prefix.length);
-    }
-    return url;
-}
+    return url.startsWith(DEFAULT_IPFS_URL) ? url.substring(DEFAULT_IPFS_URL.length) : url;
+  }
 
-const hasAvatarAttribute = item.metadata?.attributes?.some(attr => attr.trait_type === "avatar");
-const avatarAttribute = item.metadata?.attributes?.find(attr => attr.trait_type === "avatar");
+  const avatarAttribute = item.metadata.attributes.find(attr => attr.trait_type === "avatar");
 
-
- 
-  //if select is pressed we want to add it to the search params how to do that 
   return (
-    <section
-      className={`rounded-sm shadow-custom bg-background-layer-1 p-0 min-w-[150px] max-w-[200px] border `}
-      
-    >
+    <section className="rounded-sm shadow-custom bg-background-layer-1 p-0 min-w-[150px] max-w-[200px] border">
       <div className="w-full aspect-square relative m-0 object-scale-down ">
-        {hasAvatarAttribute && avatarAttribute && (
-           <Switch
-           className="absolute top-5 right-5 z-10 text-white hover:text-text-primary cursor-pointer"
-           checked={selected}
-           onCheckedChange={() => {
-             router.push(pathname + '?' + createQueryString(avatarAttribute.value, removeIPFSPrefix(item?.metadata?.image)))
-           }}
-         />
+        {avatarAttribute && (
+          <Switch
+            className="absolute top-5 right-5 z-10 text-white hover:text-text-primary cursor-pointer"
+            checked={selected}
+            onCheckedChange={() => {
+              router.push(pathname + '?' + createQueryString(avatarAttribute.value, removeIPFSPrefix(item.metadata.image)));
+            }}
+          />
         )}
-       
         <Image
-          src={`${item?.metadata.image ? 'https://ipfs.io/ipfs/'+item.metadata.image : "https://ipfs.io/ipfs/bafkreigodckc2cne7l6kcirr7hzzcpa7q7yes7xj72wl7t4lb44xfx4r7e" }`}
+          src={item.metadata.image ? DEFAULT_IPFS_URL + item.metadata.image : DEFAULT_IPFS_URL + "bafkreigodckc2cne7l6kcirr7hzzcpa7q7yes7xj72wl7t4lb44xfx4r7e"}
           alt="nft_list_item"
           className="object-scale-down rounded-t-sm bg-gradient-to-b from-blue-500 to-green-300"
           fill={true}
         />
       </div>
       <div className="flex flex-col gap-2 p-4">
-        <Link className="truncate text-text-primary text-center text-ellipsis hover:text-green-800 " href={`/nft/${item.group?.id}/${item.tokenId}`}>
-         {item?.metadata.name ? item.metadata.name : "undefined"}
+        <Link className="truncate text-text-primary font-medium text-center capitalize text-ellipsis hover:text-green-800 " href={`/nft/${item.group?.id}/${item.tokenId}`}>
+          {item.metadata.name || "undefined"}
         </Link>
-        <Badge variant="info">{item.metadata.category ? item.metadata.category : "not found"}</Badge>
+        <Badge variant="info" className="flex justify-center text-sm py-2 text-center capitalize">{item.metadata.category || "not found"}</Badge>
       </div>
     </section>
-    
-    
   );
 }
-
-
