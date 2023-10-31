6a08753941ad8020b199bc850526d88a2e7a401a
koenschuite <tse.lao@yahoo.com>
2023-10-31 08:43:42 +0100

    fixed server side rendering profile-details

--- app/profiles/[id]/layout.tsx
+++ app/profiles/[id]/layout.tsx
@@ -1,52 +1,42 @@
-"use client";
+
 
 import React from "react";
-import { useAccount } from "wagmi";
 
 import ProfileDetailCard from "@/app/profile/profile-detail-card";
-import Loading from "@/components/loading";
 import { Card } from "@/components/ui/card";
 import { defaultAvatar } from "@/lib/constants";
-import useFetchData from "@/lib/useFetchData";
-import { useParams } from "next/navigation";
-import { getAddress } from "viem";
+import { getUserProfile } from "@/lib/data/user";
 import AvatarNFTs from "../../profile/avatar-nfts";
 
-export default function ProfileViewLayout({
+export default  async function ProfileViewLayout({
+  params,
   children,
 }: {
+  params: { id: string };
   children: React.ReactNode;
 }) {
-  const params = useParams();
 
-  const { address } = useAccount();
 
-  let {
-    data: profile,
-    error,
-    loading,
-  } = useFetchData<any>(`/polybase/${getAddress(params.id.toString())}`);
+  const profile = await getUserProfile(params.id)
+
+  console.log(profile)
   
 
-  if (loading) return <Loading />;
-  if (error) return <div>error</div>;
-  if (!profile) return <div>no profile</div>;
-  profile = profile.message;
   return (
     <main className="m-8">
       <div className="absolute bg-[url('/landing/background-card.png')] object-fit z-[-10] top-[64px] left-0  min-h-[20vh] min-w-full"></div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 grid-cols-1 gap-8 align-center relative">
         <div className="col-span-1  flex flex-col gap-4">
-          {profile && profile.TBA ? (
+          {profile.message && profile.message.TBA ? (
             <ProfileDetailCard
-              profile={profile}
-              image={profile.nft ? profile.nft : defaultAvatar}
+              profile={profile.message}
+              image={profile.message.nft ? profile.message.nft : defaultAvatar}
             />
           ) : (
-            address && <Card>No TBA Account</Card>
+            params.id && <Card>No TBA Account</Card>
           )}
-          {profile && profile.nft?.length > 0 && (
-            <AvatarNFTs nfts={profile.nft} />
+          {profile && profile.message.nft?.length > 0 && (
+            <AvatarNFTs nfts={profile.message.nft} />
           )}
         </div>
         <div className="flex flex-col gap-6 lg:col-span-2">{children}</div>
--- app/profiles/[id]/page.tsx
+++ app/profiles/[id]/page.tsx
@@ -1,12 +1,5 @@
-"use client";
 
 
-
-import { useAccount } from "wagmi";
-
-
-import useFetchData from "@/lib/useFetchData";
-import { useParams } from "next/navigation";
 import ProfileDetails from "../../profile/profile-details";
 import ProfileItems from "../../profile/profile-items";
 
@@ -14,30 +7,7 @@ import ProfileItems from "../../profile/profile-items";
 //categories 
 
 
-export default function ProfileMe() {
-
-  const {id} = useParams()
-  const { address } = useAccount();
-  
-  const {data, loading} = useFetchData<any>("/nft/all/" + address);
-  
-  const {data:profileData, error:profileError, loading:profileLoading} =useFetchData<any>("/polybase/" + address);
-
-
-  
- 
-  
-  const categories = [
-    "Education", "Hackathon", "Project", "Experience", "Community", "Skills", "Other"
-  ]
-
-
-
-  //we goging to makes ure taht we have a profile to be minded 
-  
-  if(loading){
-    return <div>Loading...</div>
-  }
+export default function ProfileMe({params}: {params: {id: string}}) {
 
   return (
           <>
