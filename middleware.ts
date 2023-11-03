98eaccfcb7ebe254a8d5788b5bced897e1d70eda
koenschuite <tse.lao@yahoo.com>
2023-11-03 15:56:43 +0100

    fixes for loading images

--- middleware.ts
+++ middleware.ts
@@ -61,5 +61,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|landing|public).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|landing|public|icons|confetti|parts).*)"],
 };
