e1d8020ef0a9df185e527b26c572b9f6b8e0cf59
imomo <user@userui-MacBookPro.local>
2023-11-30 17:06:30 +0900

    add: make Profile Edit page

--- app/(onboard)/[lang]/mypage/EditProfile.tsx
+++ app/(onboard)/[lang]/mypage/EditProfile.tsx
@@ -14,7 +14,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
+import Image from "next/image";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Command,
@@ -36,6 +36,7 @@ import { ArrowDown, CheckIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useAccount } from "wagmi";
+import { Textarea } from "@/components/ui/textarea";
 
 type Job = {
   id: number;
@@ -63,6 +64,21 @@ type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {};
+
+const logos = [
+  { src: "/icons/identity_google.png", alt: "Icon 1", text: "구글 계정" },
+  { src: "/icons/identity_phone.png", alt: "Icon 2", text: "전화번호" },
+  { src: "/icons/identity_telegram.png", alt: "Icon 3", text: "텔레그램" },
+  { src: "/icons/identity_mail.png", alt: "Icon 4", text: "이메일" },
+  { src: "/icons/identity_bitcoin.png", alt: "Icon 1", text: "탈중앙화 개인지갑" },
+  { src: "/icons/identity_github.png", alt: "Icon 2", text: "Github" },
+  { src: "/icons/identity_figma.png", alt: "Icon 3", text: "피그마" },
+  { src: "/icons/identity_pinterest.png", alt: "Icon 4", text: "핀터레스트" },
+  { src: "/icons/identity_youtube.png", alt: "Icon 1", text: "유튜브" },
+  { src: "/icons/identity_linkedIn.png", alt: "Icon 2", text: "링크드인" },
+  { src: "/icons/identity_facebook.png", alt: "Icon 3", text: "페이스북" },
+  { src: "/icons/identity_reddit.png", alt: "Icon 4", text: "레딧" },
+];
 
 export default function EditProfile() {
   const form = useForm<ProfileFormValues>({
@@ -133,115 +149,95 @@ export default function EditProfile() {
   }
 
   return (
-    <Form {...form}>
-      <section className="mb-8">
-        <h1 className="text-4xl font-medium mb-6 text-primary">프로필 수정</h1>
-      </section>
-      <section className="my-2">
-        <label className="">이메일</label>
-        <div className="flex gap-2 items-center mt-2">
-          <Input placeholder="email" />
-          <Button className="w-[150px]">인증완료</Button>
+    <>
+      <Form {...form}>
+        <section className="mb-8 mr-10">
+          <h1 className="text-2xl font-medium mb-6 text-primary">
+            프로필 수정
+          </h1>
+        </section>
+        <section className="my-2 mr-10">
+          <div className="flex">
+            <div className="flex flex-col basis-1/2">
+              <div>
+                <label className="">본명 (비공개)</label>
+                <div className="flex gap-2 items-center mt-2">
+                  <Input placeholder="이름" />
+                  <Input placeholder="성" />
+                </div>
+              </div>
+              <div className="my-10">
+                <label className="">닉네임</label>
+                <div className="flex gap-2 items-center mt-2">
+                  <Input placeholder="다른 유저에게 공개될 별명을 입력해주세요." />
+                </div>
+              </div>
+            </div>
+            <div className="basis-1/2 ml-6">
+              <div>
+                <label className="">직업</label>
+                <div className="flex gap-2 items-center mt-2">
+                  <Input placeholder="개발자" />
+                  {/* <Dropdown placeholder="개발자" options={jobList} /> */}
+                </div>
+              </div>
+              <div className="my-10">
+                <label className="">전문분야</label>
+                <div className="flex gap-2 items-center mt-2">
+                  <Input placeholder="프론트엔드 개발" />
+                  {/* <Dropdown placeholder="개발자" options={jobList} /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="my-2 mr-10">
+          <div>
+            <label>내 소개</label>
+            <div className="flex gap-2 items-center mt-2">
+              <Textarea
+                placeholder="익명으로 나를 소개할 정보를 입력해주세요. (이름, 연락처) 등의 개인정보를 입력할 경우, 강제 삭제될 수 있습니다."
+                style={{ height: "200px" }}
+              />
+            </div>
+          </div>
+        </section>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 my-8 mr-10"
+        >
+          <div className="flex items-center w-full">
+            <Button
+              type="submit"
+              className="w-full h-[64px] text-1xl"
+              disabled={loading}
+              loading={loading}
+            >
+              저장하기
+            </Button>
+          </div>
+        </form>
+      </Form>
+      {/* 아래 */}
+      <div>
+        <h1 className="text-2xl font-medium mb-6 text-primary">
+          아이덴티티 인증
+        </h1>
+        {/* 4개씩 3줄짜리 테이블 */}
+        <div className="flex flex-wrap">
+          {logos.map((logo, i) => (
+            <div key={i} className="flex basis-1/4">
+              <div className="flex justify-center m-4">
+                <Image src={logo.src} width={50} height={50} alt={logo.alt} />
+              </div>
+              <div className="flex flex-col justify-center m-2">
+                <p className="text-md">{logo.text}</p>
+                <p className="text-sm text-gray-400 mt-1">인증하기</p>
+              </div>
+            </div>
+          ))}
         </div>
-      </section>
-      <Alert variant={"info"} className="my-8">
-        <AlertTitle className="text-state-info">
-          완벽한 개인정보 보안
-        </AlertTitle>
-        <AlertDescription>
-          모든 개인정보는 암호화를 통해 저장되어 비공개 상태가 유지되요. 본인의
-          승인하에만 상대방에게 공개될 수 있어요.
-        </AlertDescription>
-      </Alert>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 my-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter a username" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="job"
-          render={({ field }) => (
-            <FormItem className="flex flex-col w-full">
-              <FormLabel>Job Type</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        "w-full justify-between rounded-sm",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value
-                        ? jobList.find((job) => job.id === field.value?.id)
-                            ?.name
-                        : "Select job type"}
-                      <ArrowDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-full p-0">
-                  <Command className="w-full ">
-                    <CommandInput placeholder="Search job..." />
-                    <CommandEmpty>Nothing found.</CommandEmpty>
-                    <CommandGroup className="w-full max-h-[40vh] overflow-auto">
-                      {jobList.map((job: Job) => (
-                        <CommandItem
-                          value={job.name}
-                          key={job.name}
-                          className="my-4 w-[400px]"
-                          onSelect={() => {
-                            form.setValue("job", job);
-                          }}
-                        >
-                          <CheckIcon
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              job.name === field.value?.name
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                          <div className="flex flex-col gap-2">
-                            <h5 className="font-medium">{job.name}</h5>
-                            <span className="text-text-secondary">
-                              {job.groupName}
-                            </span>
-                          </div>
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <div className="flex items-center w-full">
-          <Button
-            type="submit"
-            className="w-full"
-            disabled={loading}
-            loading={loading}
-          >
-            저장하기
-          </Button>
-        </div>
-      </form>
-    </Form>
+      </div>
+    </>
   );
 }
