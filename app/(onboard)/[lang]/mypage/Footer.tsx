5455d94dab7d36fafa87ee3c15f8119c6fe76a98
imomo <user@userui-MacBookPro.local>
2023-11-30 17:05:28 +0900

    add: make footer component

--- app/(onboard)/[lang]/mypage/Footer.tsx
+++ app/(onboard)/[lang]/mypage/Footer.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col h-[340px] justify-between items-center">
+    <div className="flex flex-col h-[320px] justify-between items-center">
       <Image
         src={`/images/careerzen.png`}
         width={400}
-        height={400}
+        height={300}
         alt={"Logo"}
       />
       <div className="flex gap-4 text-md text-gray-500">
@@ -16,24 +16,26 @@ const Footer = () => {
         <p>개인정보처리방침</p>
         <p>이용약관</p>
       </div>
-      <div className="flex gap-4">
-        <Image
-          src={`/icons/discord.png`}
-          height={24}
-          width={24}
-          alt={"Discord"}
-        />
-        <Image
-          src={`/icons/telegram.png`}
-          height={24}
-          width={24}
-          alt={"Discord"}
-        />
-        <Image src={`/icons/x.png`} height={24} width={24} alt={"Discord"} />
+      <div className="grid place-content-center justify-center justify-items-center">
+        <div className="flex gap-4">
+          <Image
+            src={`/icons/discord.png`}
+            height={24}
+            width={24}
+            alt={"Discord"}
+          />
+          <Image
+            src={`/icons/telegram.png`}
+            height={24}
+            width={24}
+            alt={"Discord"}
+          />
+          <Image src={`/icons/x.png`} height={24} width={24} alt={"Discord"} />
+        </div>
+        <p className="text-gray-300 text-md m-6">
+          Copyright © 2023. Careerzen. All rights reserved.
+        </p>
       </div>
-      <p className="text-gray-300 text-md m-0">
-        Copyright © 2023. Careerzen. All rights reserved.
-      </p>
     </div>
   );
 };
