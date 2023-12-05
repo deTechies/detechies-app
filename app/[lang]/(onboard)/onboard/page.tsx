135fcde18ddef97cdb5400f5b53a74f9d7501af5
koenschuite <tse.lao@yahoo.com>
2023-12-05 17:16:39 +0100

    remove dictionary for testing error

--- app/[lang]/(onboard)/onboard/page.tsx
+++ app/[lang]/(onboard)/onboard/page.tsx
@@ -1,6 +1,6 @@
-import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n.config";
 import Image from "next/image";
+import LoginButtons from "./login-buttons";
 
 export default async function OnboardPage({
   params: { lang },
@@ -8,7 +8,7 @@ export default async function OnboardPage({
   params: { lang: Locale };
 }) {
   
-  const dictionary = await getDictionary(lang);
+  //const dictionary = await getDictionary(lang);
   
   return (
     <div className="flex flex-col gap-8 items-center">
@@ -19,17 +19,20 @@ export default async function OnboardPage({
         alt={`careerzen Logo`}
       />
       <h1 className="text-xl font-medium tracking-wider text-center">
-        {
+        {/* {
           dictionary.onboard.welcome.title
-        }
+        } */}
+        Welcome
       </h1>
-      <h5 className="text-text-secondary font-light text-center">
+    {/*   <h5 className="text-text-secondary font-light text-center">
         {
           dictionary.onboard.welcome.body
         }
-      </h5>
+      </h5> */}
       <div className="flex flex-col space-y-1 gap-4 w-full">
-        
+        <LoginButtons 
+
+        />
       </div>
     </div>
   );
