dfcdbe46bff98e7defac5fae2da3b63c935b4567
koenschuite <tse.lao@yahoo.com>
2023-12-06 18:37:25 +0100

    check connection token:

--- app/[lang]/(app)/settings/page.tsx
+++ app/[lang]/(app)/settings/page.tsx
@@ -4,10 +4,26 @@ import { getSessionToken } from "@/lib/data/project"
 export default  async function SettingsPAge() {
 
     const session  =await getSessionToken()
+    
+    const moreDAta = await fetch(`"https://api.careerbadge.online/users/${session?.web3.address}`, 
+    {
+        headers: {
+            "Authorization": `Bearer ${session?.web3.accessToken}`
+        }
+    })
+    
+    
   return (
+    <main>
     <div>{
         JSON.stringify(
             session
         )}</div>
+          <div>{
+        JSON.stringify(
+            moreDAta
+        )}</div>
+        
+        </main>
   )
 }
