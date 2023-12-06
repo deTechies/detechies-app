7ebab12de1acae47e084cf8b89fc4c42ee41cada
koenschuite <tse.lao@yahoo.com>
2023-12-06 13:38:37 +0100

    fix

--- app/[lang]/(app)/profiles/list-profiles.tsx
+++ app/[lang]/(app)/profiles/list-profiles.tsx
@@ -1,40 +1,47 @@
-"use client"
+"use client";
 
 import ProfileCard from "@/components/card/profile-card";
 import { useSearchParams } from "next/navigation";
 
-
 export interface Profile {
-    id: string;
-    name: string;
-    email: string;
-    display_name: string;
-    avatar: string;
-    image: string;
-    nft: string[];
+  id: string;
+  name: string;
+  email: string;
+  display_name: string;
+  avatar: string;
+  image: string;
+  nft: string[];
 }
-export default function ListProfiles({users, followers}: {users: Profile[], followers:string[]}) {
-
+export default function ListProfiles({
+  users,
+  followers,
+}: {
+  users: Profile[];
+  followers: string[];
+}) {
   const searchParams = useSearchParams()!;
-    
-  const nameFilter =
-    searchParams.get("search") ||
-    "";
+
+  const nameFilter = searchParams.get("search") || "";
 
   return (
-    
     <div className="w-full md:m-24 m-8 grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-
-            {users?.length > 0 && users
-            .filter((profile: Profile) => {
-                return (
-                    (nameFilter === '' || profile.display_name.toLowerCase().includes(nameFilter.toLowerCase()))                 );
-            })
-            .map((profile:Profile, index) => (
-                <ProfileCard key={index} profile={profile} followed={followers && followers.includes(profile.id)}/>
-            ))
-            }
-        </div>
-
-  )
+      {users?.length > 0 &&
+        users
+          .filter((profile: Profile) => {
+            return (
+              nameFilter === "" ||
+              profile.display_name
+                .toLowerCase()
+                .includes(nameFilter.toLowerCase())
+            );
+          })
+          .map((profile: Profile, index) => (
+            <ProfileCard
+              key={index}
+              profile={profile}
+              followed={followers && followers.includes(profile.id)}
+            />
+          ))}
+    </div>
+  );
 }
