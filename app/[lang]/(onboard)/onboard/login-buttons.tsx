16064a1522915d36f267aabeba88c8a12ca43810
koenschuite <tse.lao@yahoo.com>
2023-12-13 11:59:47 +0100

    temp remove sesison from login

--- app/[lang]/(onboard)/onboard/login-buttons.tsx
+++ app/[lang]/(onboard)/onboard/login-buttons.tsx
@@ -3,9 +3,8 @@ import { Button } from "@/components/ui/button";
 import { toast } from "@/components/ui/use-toast";
 import { polygonMumbai } from "@/helpers/mumbai";
 import { truncateMiddle } from "@/lib/utils";
-import { getCsrfToken, signIn, useSession } from "next-auth/react";
+import { getCsrfToken, signIn } from "next-auth/react";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { SiweMessage } from "siwe";
 import { useAccount, useConnect, useDisconnect, useSignMessage } from "wagmi";
@@ -13,7 +12,7 @@ import { useAccount, useConnect, useDisconnect, useSignMessage } from "wagmi";
 export default function LoginButtons({ text }: { text?: any }) {
   const { connect, connectors } = useConnect();
   const { address } = useAccount();
-  const { data: session } = useSession();
+ // const { data: session } = useSession();
   const { signMessageAsync } = useSignMessage();
   const [signing, setSigning] = useState(false);  
   const { disconnect } = useDisconnect();
@@ -21,7 +20,7 @@ export default function LoginButtons({ text }: { text?: any }) {
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
 
-  useEffect(() => {
+/*   useEffect(() => {
     if (session && session.web3?.accessToken) {
       if (session.web3.address == address) {
         if (session.web3.user?.verified) {
@@ -31,7 +30,7 @@ export default function LoginButtons({ text }: { text?: any }) {
       }
     }
   }, [address, session]);
-
+ */
   const handleConnect = (connector: any) => {
     connect({ connector });
   };
