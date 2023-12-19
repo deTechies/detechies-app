4d8ce63e016839af42971dca414b8fb7e149c421
Jiyong Kim <gd693020@gmail.com>
2023-12-20 08:16:34 +0900

    test lang

--- app/[lang]/(app)/project/page.tsx
+++ app/[lang]/(app)/project/page.tsx
@@ -11,8 +11,11 @@ import {
 } from "@/components/ui/select";
 import { getProjects } from "@/lib/data/project";
 
+import { getDictionary } from "@/get-dictionary";
+import { Locale } from "@/i18n.config";
+
 import { ProjectType, PrivacyType } from "@/lib/interfaces";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 import CreateProject from "./create-project";
 import ProjectItem from "./project-item";
@@ -35,8 +38,10 @@ export interface ProjectItemProps {
 
 export default async function ProjectListPage({
   searchParams,
+  params,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
+  params: { lang: Locale };
 }) {
   const projects = await getProjects();
 
@@ -47,22 +52,25 @@ export default async function ProjectListPage({
       )
     : projects;
 
-  return (
+  const dictionary = (await getDictionary(params.lang)) as any;
 
+  return (
     <main className="flex flex-col gap-6 w-full my-4 p-4 mx-auto">
       <Card className="flex gap-5 justify-between pt-7 px-8 pb-8">
-        <h1 className="text-subhead_s">Projects</h1>
+        <h1 className="text-subhead_s">{dictionary.project.list.projects}</h1>
         <div className="flex justify-between sm:flex-row flex-col">
           <div className="flex gap-5 items-center sm:flex-row flex-col">
             <Select>
               <SelectTrigger className="w-[180px] px-3 py-3.5">
-                <SelectValue placeholder="All projects" />
+                <SelectValue
+                  placeholder={dictionary.project.list.all_project}
+                />
               </SelectTrigger>
 
               <SelectContent>
                 {Object.values(ProjectType).map((type) => (
                   <SelectItem key={type} value={type}>
-                    {type}
+                    {dictionary.project.list.project_type[type]}
                   </SelectItem>
                 ))}
               </SelectContent>
@@ -70,44 +78,49 @@ export default async function ProjectListPage({
 
             <Select>
               <SelectTrigger className="w-[180px] px-3 py-3.5">
-                <SelectValue placeholder="Public" />
+                <SelectValue
+                  placeholder={dictionary.project.list.scope_of_disclosure}
+                />
               </SelectTrigger>
 
               <SelectContent>
                 {Object.values(PrivacyType).map((type) => (
                   <SelectItem key={type} value={type}>
-                    {type}
+                    {dictionary.project.list.privacy_type[type]}
                   </SelectItem>
                 ))}
               </SelectContent>
             </Select>
 
-            <Search placeholder="search" />
+            <Search placeholder={dictionary.project.list.search} />
             <div className="flex items-center gap-3">
               <Checkbox />
-              <Label className="text-title_m">My Projects</Label>
+              <Label className="text-title_m">
+                {dictionary.project.list.my_project}
+              </Label>
             </div>
           </div>
           <CreateProject />
         </div>
       </Card>
-<Suspense fallback={<div>Loading...</div>}>
-      <section className="w-full grid md:grid-cols-2 gap-4">
-        {filteredData.length > 0 ? (
-          filteredData.map((item: ProjectItemProps) => (
-            <ProjectItem key={item.id} details={item} />
-          ))
-        ) : (
-          <div>No projects found</div>
-        )}
-      </section>
 
-      {filteredData.length > 0 && (
-        <Button size="lg" variant="secondary">
-          View More
-        </Button>
-      )}
-        </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <section className="w-full grid md:grid-cols-2 gap-4">
+          {filteredData.length > 0 ? (
+            filteredData.map((item: ProjectItemProps) => (
+              <ProjectItem key={item.id} details={item} />
+            ))
+          ) : (
+            <div>{dictionary.project.list.no_projects_found}</div>
+          )}
+        </section>
+
+        {filteredData.length > 0 && (
+          <Button size="lg" variant="secondary">
+            {dictionary.project.list.view_more}
+          </Button>
+        )}
+      </Suspense>
     </main>
   );
 }
