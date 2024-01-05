58c720e83412ba810ddf20852b56065242fadb5b
koenschuite <tse.lao@yahoo.com>
2024-01-05 11:43:35 +0100

    signout if user is not detected

--- app/[lang]/(app)/layout.tsx
+++ app/[lang]/(app)/layout.tsx
@@ -52,10 +52,9 @@ export default async function RootLayout({
   const session = await auth();
   
   if(!session){
-
     redirect("/onboard");
   }
-  if(!session?.web3?.user?.verified) {
+  if(!session?.web3?.user) {
     signOut();
     redirect("/onboard");
   }
