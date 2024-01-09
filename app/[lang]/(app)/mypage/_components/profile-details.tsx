219fe782de7def85a3d163ec04112189d2497299
Cy <notcyanpascual@gmail.com>
2024-01-09 10:52:00 +0800

    Fix: Changed badge light colors to match design document

--- app/[lang]/(app)/mypage/_components/profile-details.tsx
+++ app/[lang]/(app)/mypage/_components/profile-details.tsx
@@ -19,18 +19,18 @@ export default async function ProfileDetails({
         <header className="flex justify-between items-center">
             <h5 className="text-subhead_s capitalize">{text?.profile}</h5>
             <Link href="/mypage/edit">
-              <div className="flex bg-slate-200 text-black text-xs font-medium me-2 px-3 py-1 rounded-md dark:bg-slate-100 dark:text-slate-500 capitalize">
+              <div className="flex bg-[#EFF0F2] text-[#101113] text-xs font-medium me-2 px-[10px] py-[6px] rounded-[20px] dark:bg-slate-100 dark:text-slate-500 capitalize">
                 {text?.edit} <Edit className='ml-2 text-text-secondary' size='12'/>
               </div>
             </Link>
         </header>
         <div className="flex">
-          <div className="border-[1px] border-blue-400 text-blue-500 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:border-0 dark:bg-blue-900 dark:text-blue-300">
+          <div className="border-[1px] border-[#0099FF] text-[#2E94FF] text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:border-0 dark:bg-blue-900 dark:text-blue-300">
             {profile.profile_details?.profession as string}
           </div>
           {profile.profile_details?.skills.map((skill:string)=>{
             return(
-              <div className="border-[1px] border-green-500 text-green-500 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:border-0 dark:bg-green-900 dark:text-green-300">
+              <div className="border-[1px] border-[#00D41D] text-[#00D41D] text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:border-0 dark:bg-green-900 dark:text-green-300">
                 {skill}
               </div>
             )
