23e2e53593eee61f3efece757ad298b573298039
koenschuite <tse.lao@yahoo.com>
2024-01-10 11:21:53 +0900

    fixes for project
    fixes for proejct details translation issues

--- app/[lang]/(app)/mypage/avatar/page.tsx
+++ app/[lang]/(app)/mypage/avatar/page.tsx
@@ -12,6 +12,22 @@ export default async function Avatars() {
   const achievements = await getUserAchievements();
   const lang = await getDictionary("kr");
 
+  //getting all that has the status pending
+  //and then display them in the pending tab
+  //if the user is the owner of the achievement
+  //then display them in the career tab
+
+   const pending = achievements.filter((achievement: AchievementReward) => {
+    return achievement.status == "requested";
+  });
+
+/*   const avatar = achievements.filter((achievement: AchievementReward) => {
+    return (
+      achievement.status == "granted" &&
+      achievement.achievement.type == "avatar"
+    );
+  });  */
+
   return (
     <Tabs defaultValue="all">
       <TabsList className="flex items-start justify-start w-full gap-4">
@@ -35,11 +51,17 @@ export default async function Avatars() {
             )}
         </div>
       </TabsContent>
-      <TabsContent value="career">
-        testing
-      </TabsContent>
+      <TabsContent value="career">testing</TabsContent>
       <TabsContent value="minting">
-         Requested by users
+        {achievements &&
+          pending.map((achievementReward: AchievementReward, index: number) => (
+            <DisplayNFT
+              key={index}
+              details={achievementReward.achievement}
+              showSelect={true}
+              lang={lang}
+            />
+          ))}
       </TabsContent>
     </Tabs>
   );
