f1223c9536fb9ac8d9d388fa133bdf37eb410951
koenschuite <tse.lao@yahoo.com>
2024-01-11 13:34:57 +0900

    CAR-209 update middleware + profile
    added option for user to switch the language in user profile modal + makes sure it stay the same language.

--- components/user/profile-details.tsx
+++ components/user/profile-details.tsx
@@ -7,8 +7,15 @@ import { Address, createPublicClient, http } from "viem";
 
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 import { useToast } from "../ui/use-toast";
 import ModalLayout from "./modal-layout";
 import ProfileBalance from "./profile-balance";
@@ -20,14 +27,15 @@ export default function ProfileDetails({ showModal }: any) {
   const router = useRouter();
   const { chain, chains } = useNetwork();
   const [balances, setBalances] = useState<any>([]);
+  const pathName = usePathname();
+  const params = pathName.split("/")[1];
 
   const { address: account, isConnected } = useAccount();
-  
-  const {data:session} = useSession();
+
+  const { data: session } = useSession();
 
   useEffect(() => {
     async function getBalances() {
-
       for (let i = 0; i < chains.length; i++) {
         const client = createPublicClient({
           chain: chains[i],
@@ -72,6 +80,14 @@ export default function ProfileDetails({ showModal }: any) {
     toast({ title: "Copied to clipboard" });
   }
 
+  function changeLanguage(value: string) {
+    //change the value in the path name
+    const segments = pathName.split("/").filter(Boolean);
+    const newPath = "/" + value + "/" + segments.slice(1).join("/");
+    router.replace(newPath);
+  }
+
+
   return (
     <ModalLayout title="My Account" showModal={showModal}>
       {isConnected ? (
@@ -80,26 +96,31 @@ export default function ProfileDetails({ showModal }: any) {
             id="username"
             className="text-md bg-background-layer-2 rounded-md p-4"
           >
-            <span className="text-text-secondary text-label_m">내 암호화 계정 주소</span>
+            <span className="text-text-secondary text-label_m">
+              내 암호화 계정 주소
+            </span>
             <div className="flex gap-2 items-center justify-between flex-wrap">
               <span className="text-xs flex-wrap">{account && account}</span>
-              
-              {
-                session?.web3?.address != account ? (
-                  <Button
-                    size="sm"
-                    variant={"destructive"}
-                    className="text-md shrink-0"
-                    onClick={() => signOut()}
-                  >
-                    Change Account
-                  </Button>
-                ) : (
-                  <Button variant="secondary" size="sm" className="shrink-0" onClick={copyAddress}>
-                  복사하기  
-                </Button>)
-              }
-             
+
+              {session?.web3?.address != account ? (
+                <Button
+                  size="sm"
+                  variant={"destructive"}
+                  className="text-md shrink-0"
+                  onClick={() => signOut()}
+                >
+                  Change Account
+                </Button>
+              ) : (
+                <Button
+                  variant="secondary"
+                  size="sm"
+                  className="shrink-0"
+                  onClick={copyAddress}
+                >
+                  복사하기
+                </Button>
+              )}
             </div>
           </div>
 
@@ -115,21 +136,49 @@ export default function ProfileDetails({ showModal }: any) {
               />
             ))}
           </div>
-          
-          <div className="grid grid-cols-2 gap-4">
-            <Button onClick={() => {
-              disconnect()
-              signOut()
-              router.push('/onboard')
 
-            }} variant="destructive">
+          <div className="flex justify-between mb-4">
+            <Select
+              onValueChange={(value) => changeLanguage(value)}
+              defaultValue={params}
+            >
+              <SelectTrigger className="w-[105px] bg-background-layer-1 border">
+                <SelectValue placeholder="Lang" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="kr">한국어</SelectItem>
+                <SelectItem value="en">English</SelectItem>
+              </SelectContent>
+            </Select>
+
+            <Select>
+              <SelectTrigger className="w-[100px]  bg-background-layer-1 border">
+                <SelectValue placeholder="Theme" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4">
+            <Button
+              onClick={() => {
+                disconnect();
+                signOut();
+                router.push("/onboard");
+              }}
+              variant="destructive"
+            >
               Sign out
             </Button>
             <Link
               href={`/mypage`}
               className="bg-accent-secondary text-accent-primary hover:bg-accent-secondary/50 w-full rounded-md flex items-center justify-center"
             >
-                Visit Profile
+              Visit Profile
             </Link>
           </div>
         </div>
