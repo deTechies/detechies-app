b14c231e5b6dbd9e3913f4c1edca13e6a35c02a4
koenschuite <tse.lao@yahoo.com>
2024-01-13 15:37:07 +0900

    fix for group not working

--- lib/data/groups.ts
+++ lib/data/groups.ts
@@ -17,7 +17,7 @@ export async function getGroups(search?: string) {
 }
 
 export async function getClub(clubId: string) {
-  const session = auth() as any;
+  const session = (await getServerSession(authOptions)) as any;
 
   if (!session?.web3?.accessToken) {
     throw new Error("Session not found or address missing in session");
