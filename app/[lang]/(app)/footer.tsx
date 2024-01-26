f2800fe1bcf03974f8c97da1e441538b031cd651
Jiyong Kim <gd693020@gmail.com>
2024-01-26 15:49:48 +0900

    Update footer.tsx

--- app/[lang]/(app)/footer.tsx
+++ app/[lang]/(app)/footer.tsx
@@ -7,41 +7,39 @@ export default async function Footer({ lang }: { lang: any }) {
     <div className="flex flex-col justify-center items-center mx-auto w-full my-[32px] gap-8 px-4">
       <Image
         src={`/images/careerzen.png`}
-        className="dark:hidden max-w-[360px]"
-        layout="responsive"
+        className="dark:hidden max-w-[360px] w-full"
         width={360}
         height={60}
         alt={"Logo"}
       />
       <Image
         src={`/images/logo-invert.png`}
-        className="hidden dark:block max-w-[360px]"
-        layout="responsive"
+        className="hidden dark:block max-w-[360px] w-full"
         width={360}
         height={60}
         alt={"Logo"}
       />
-      <div className="flex flex-wrap justify-center items-center text-title_m text-text-secondary my-5">
-        <Link href="#" className=" hover:text-accent-primary px-4">
+      <div className="flex flex-wrap items-center justify-center my-5 text-title_m text-text-secondary">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.terms_and_conditions}
         </Link>
-        <Link href="#" className=" hover:text-accent-primary px-4">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.faq}
         </Link>
-        <Link href="#" className=" hover:text-accent-primary px-4">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.privacy_policy}
         </Link>
-        <Link href="#" className=" hover:text-accent-primary px-4">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.whitepaper}
         </Link>
-        <Link href="#" className=" hover:text-accent-primary px-4">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.contact_us}
         </Link>
-        <Link href="#" className=" hover:text-accent-primary px-4">
+        <Link href="#" className="px-4  hover:text-accent-primary">
           {lang.footer.user_guide}
         </Link>
       </div>
-      <div className="grid place-content-center justify-center justify-items-center gap-8">
+      <div className="grid justify-center gap-8 place-content-center justify-items-center">
         <div className="flex gap-4">
           <Link
             href="https://open.kakao.com/o/gGdZ76If"
