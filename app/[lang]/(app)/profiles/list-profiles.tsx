d86916470492d40edb91ed566659d15f925313c9
koenschuite <tse.lao@yahoo.com>
2024-01-27 14:55:24 +0900

    Update list-profiles.tsx

--- app/[lang]/(app)/profiles/list-profiles.tsx
+++ app/[lang]/(app)/profiles/list-profiles.tsx
@@ -1,33 +1,31 @@
-"use client";
-
 import ProfileCard from "@/components/card/profile-card";
+import { serverApi } from "@/lib/data/general";
 import { User } from "@/lib/interfaces";
-import { useSearchParams } from "next/navigation";
 
-export default function ListProfiles({
-  users,
+export default async function ListProfiles({
   lang,
+  searchParams,
 }: {
-  users: User[];
   lang: any;
+  searchParams: { [key: string]: string | undefined };
 }) {
-  const searchParams = useSearchParams()!;
+  // lets get all the users profiles here..
+  const role = searchParams.role;
+  const newUrl = new URLSearchParams();
+  if (role) {
+    newUrl.set("role", role);
+  }
+  if (searchParams.search) {
+    newUrl.set("display_name", searchParams.search);
+  }
 
-  const nameFilter = searchParams.get("search") || "";
+  const { data: users } = await serverApi(`/users`, newUrl.toString());
 
   return (
     <div className="w-full  grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 pr-8">
       {users?.length > 0 &&
         users
-          .filter((profile: User) => {
-            return (
-              nameFilter === "" ||
-              profile.display_name
-                .toLowerCase()
-                .includes(nameFilter.toLowerCase())
-            );
-          })
-          .map((profile: User, index) => (
+          .map((profile: User, index:number) => (
             <ProfileCard key={index} profile={profile} lang={lang} />
           ))}
     </div>
