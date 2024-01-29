4d28d3e84534729248e236974c924dc3ffc915e1
koenschuite <tse.lao@yahoo.com>
2024-01-29 12:06:09 +0900

    production fixes
    produciton fixes.
    disable wepin,
    remove report
    fix acheivement translation

--- app/[lang]/(app)/profiles/[user]/page.tsx
+++ app/[lang]/(app)/profiles/[user]/page.tsx
@@ -10,7 +10,6 @@ import ProfileProjects from "../../mypage/_components/profile-projects";
 
 import NoProfileFound from "./_component/no-profile-found";
 import UserProfile from "./_component/user-profile";
-import UserReport from "./_component/user-report";
 
 export default async function Dashboard({
   params,
@@ -28,7 +27,7 @@ export default async function Dashboard({
       {/* LeftSide */}
       <div className="min-w-[330px] flex flex-col gap-8">
         <UserProfile profile={profile} text={dictionary.mypage.profile}  />
-        <UserReport profile={profile} text={dictionary} ></UserReport>
+        {/* <UserReport profile={profile} text={dictionary} /> */}
       </div>
       {/* main */}
       <div className="grow">
--- app/[lang]/(onboard)/onboard/layout.tsx
+++ app/[lang]/(onboard)/onboard/layout.tsx
@@ -1,36 +1,35 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Locale } from "@/i18n.config";
 import type { Metadata } from "next";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
 import "../../globals.css";
 
 import { Suspense } from "react";
+import Loading from "../../(app)/loading";
 import App from "../../app";
 
- 
 // Font files can be colocated inside of `app`
 const pretendard = localFont({
   src: [
     {
-      path: '../../pretendard-regular.woff2',
-      weight: '400', // Regular
-      style: 'normal',
+      path: "../../pretendard-regular.woff2",
+      weight: "400", // Regular
+      style: "normal",
     },
     {
-      path: '../../pretendard-semibold.woff2',
-      weight: '600', // SemiBold
-      style: 'normal',
+      path: "../../pretendard-semibold.woff2",
+      weight: "600", // SemiBold
+      style: "normal",
     },
     {
-      path: '../../pretendard-bold.woff2',
-      weight: '700', // Bold
-      style: 'normal',
-    }
+      path: "../../pretendard-bold.woff2",
+      weight: "700", // Bold
+      style: "normal",
+    },
   ],
-  display: 'swap',
-})
-
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Careerzen - Build you career profile and grow with valuable insights",
@@ -45,14 +44,10 @@ export default async function RootLayout({
   params: { lang: Locale };
   children: React.ReactNode;
 }) {
-  
-
-  
   return (
     <html lang="ko" suppressHydrationWarning>
       <body
         className={`${pretendard.className} bg-background-layer-1 text-text-primary min-h-[100vh] `}
-
       >
         <ThemeProvider
           attribute="class"
@@ -60,16 +55,15 @@ export default async function RootLayout({
           enableSystem={true}
           disableTransitionOnChange={true}
         >
-          <Suspense fallback="loading app">
-          <App>
-            <main className="mx-auto max-w-lg flex items-center min-h-[100vh]">
-            {children}
-            </main>
-          </App>
+          <Suspense fallback={<Loading />}>
+            <App>
+              <main className="mx-auto max-w-lg flex items-center min-h-[100vh]">
+                {children}
+              </main>
+            </App>
           </Suspense>
         </ThemeProvider>
       </body>
     </html>
   );
 }
-
