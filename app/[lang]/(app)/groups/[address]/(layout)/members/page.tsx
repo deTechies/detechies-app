0273e81ac82279d3c103d45c7d52f756ebdb040f
Jiyong Kim <gd693020@gmail.com>
2024-02-14 15:49:17 +0900

    fix grid for group members, nfts

--- app/[lang]/(app)/groups/[address]/(layout)/members/page.tsx
+++ app/[lang]/(app)/groups/[address]/(layout)/members/page.tsx
@@ -46,7 +46,7 @@ export default async function GroupMember({
         />
       </div>
 
-      <div className="grid items-stretch gap-4 grid-cols:2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid items-stretch gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {filteredData &&
           filteredData.map((item: ClubMember, index: any) => {
             return (
