9122674570fa064def543f3178ffa82e3c434643
koenschuite <tse.lao@yahoo.com>
2024-03-02 12:12:16 +0800

    small changes regarding names

--- app/[lang]/(app)/footer.tsx
+++ app/[lang]/(app)/footer.tsx
@@ -4,7 +4,7 @@ import PrivacyPolicy from "../(onboard)/onboard/email/_components/privacy-policy
 export default async function Footer({ lang }: { lang: any }) {
   //const dictionary = (await getDictionary(params.lang)) as any;
   return (
-    <div className="flex  justify-between items-center mx-auto w-full gap-8 px-4 border-t border-border-input">
+    <div className="flex  justify-between items-center mx-auto w-full gap-8 px-4 ">
        <p className="text-center text-text-secondary text-body_s">
         {lang.footer.copywrite}
       </p>
@@ -20,13 +20,13 @@ export default async function Footer({ lang }: { lang: any }) {
           <div className="px-4 my-5 hover:text-accent-primary">{lang.footer.privacy_policy}</div>
         </PrivacyPolicy>
         {/* <Link href="#" className="px-4 my-5 hover:text-accent-primary">
-          {lang.footer.privacy_policy}
+          {lang.footer.privacy_policy} 
         </Link> */}
         <Link href="#" className="px-4 my-5 hover:text-accent-primary">
           {lang.footer.whitepaper}
         </Link>
         <Link
-          href="https://t.me/Careerzen_org"
+          href="https://t.me/+ortpABZX8vYwM2Fk"
           target="_blank"
           className="px-4 my-5 hover:text-accent-primary"
         >
