ca27bd4d503a31df9c64fd4941ed466e6bbb69f2
koenschuite <tse.lao@yahoo.com>
2024-03-07 16:11:52 +0800

    fix loading state of login-buttons
    updated the loading state of the button, to remove the switching of components.

--- app/[lang]/(onboard)/onboard/page.tsx
+++ app/[lang]/(onboard)/onboard/page.tsx
@@ -1,5 +1,6 @@
 import { Locale } from "@/i18n.config";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import Image from "next/image";
 import { Suspense } from "react";
 import LoginButtons from "./login-buttons";
@@ -9,8 +10,6 @@ export default async function OnboardPage({
 }: {
   params: { lang: Locale };
 }) {
-
-
   return (
     <main className="flex flex-col gap-12 items-center w-full md:w-[1/2] mx-auto">
       <div className="flex flex-col gap-8 w-full text-center">
@@ -26,19 +25,22 @@ export default async function OnboardPage({
           </div>
         </div>
         <div>
-          
-        <h1 className="flex flex-col gap-1 mb-2 text-xl font-medium">
-          Welcome Techie 
-        </h1>
-        <p>
-          Choose your preferred way of signing in. 
-        </p>
+          <h1 className="flex flex-col gap-1 mb-2 text-xl font-medium">
+            Welcome Techie
+          </h1>
+          <p>Choose your preferred way of signing in.</p>
         </div>
       </div>
 
       <div className="flex flex-col w-full gap-4 space-y-1">
-        <Suspense fallback={<p>Loading buttons...</p>}>
-          <LoginButtons  />
+        <Suspense
+          fallback={
+            <div className="h-[100px]">
+              <Skeleton className="h-24 grow shrink animate-pulse bg-background-layer-1" />
+            </div>
+          }
+        >
+          <LoginButtons />
         </Suspense>
       </div>
     </main>
